d8aa2c234492cf7e75fd6f9f4bfaf329692bf6d9
Francis Julian Addison <faddison@Franciss-MacBook.local>
2017-12-03 11:12:26 -0500

    created game logic 2

--- memory-game/js/script.js
+++ memory-game/js/script.js
@@ -1,6 +1,18 @@
-console.log("Up and running!");
-var cardOne = "king"
-var cardTwo = "king"
-var cardThree = "queen"
-var cardFour = "queen"
-console.log("User flipped " + cardOne);
+// console.log("Up and running!");
+// console.log("User flipped " + cardOne);
+let cards = ["queen", "queen", "king", "king"]
+let cardsInPlay = []
+var cardOne = cards[0]
+cardsInPlay.push(cardOne)
+console.log("User flipped queen")
+let cardTwo = cards[2]
+cardsInPlay.push(cardTwo)
+console.log("User flipped king")
+if (cardsInPlay.length === 2) {
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+        alert("you found a match")
+      } else {
+        alert("please try again")
+      }
+}
+   
